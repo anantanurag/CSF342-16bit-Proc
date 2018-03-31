5db3e49cdfa328af83456720a202d05db51ab8b7
khandelwalkshitij <kshitij14159@gmail.com>
2018-03-31 22:20:34 +0530

    tested ALU with single bit SAR

--- alu.v
+++ alu.v
@@ -1,3 +1,42 @@
+`timescale 1ns/10ps
+
+module tb_alu();
+
+	wire [15:0] out;
+	wire z;
+	reg [15:0] a,b;
+	reg [2:0] op;
+	reg clk;
+
+	alu uut(out,z,a,b,op,clk);
+
+	initial
+		begin
+		#00 clk = 1'b0;
+		forever #10 clk = ~clk;
+		end
+
+	initial
+		begin
+		#20  op=3'd0; a=16'd54; b=16'd5;
+		#20  op=3'd1; a=16'd54; b=16'd5;
+		#20  op=3'd2; a=16'd54; b=16'd5;
+		#20  op=3'd3; a=16'd54; b=16'd5;
+		#20  op=3'd4; a=16'd54; b=16'd5;
+		#20  op=3'd5; a=16'd54; b=16'd5;
+		#20  op=3'd6; a=16'd54; b=16'd5;
+		#20  op=3'd7; a=16'd54; b=16'd5;
+		#20  $stop;
+		end
+
+	initial
+		begin
+		$dumpfile("alu.vcd");
+		$dumpvars;
+		end
+
+endmodule
+
 module alu (	out,z,
 				a,b,
 				alu_op,
@@ -52,7 +91,7 @@ module alu (	out,z,
 					z <= (out==0);
 				end
 		SAR	:	begin
-					out <= {a[15],a[15:1]};
+					out <= {a[1],a[15:1]};
 					z <= (out==0);
 				end
 		default:begin
