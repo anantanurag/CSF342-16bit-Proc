f8c31ade0317e08a83ab99bed8b792353c5b63fd
Anant Anurag <anantquantumphysics@gmail.com>
2018-04-01 22:56:27 +0530

    Made MDR clocked

--- MemoryDataRegister.v
+++ MemoryDataRegister.v
@@ -1,8 +1,8 @@
-module MemoryDataRegister(D_OUT, D_IN);
+module MemoryDataRegister(D_OUT, D_IN, clk);
 output reg [15:0] D_OUT;
 input wire [15:0] D_IN;
-
-always@(*)
+input wire clk;
+always@(posedge clk)
 begin
 	D_OUT <= D_IN;
 end
