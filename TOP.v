8e68855d1bc5b3da96fd1a3d97d62c0d84925b35
Anant Anurag <anantquantumphysics@gmail.com>
2018-04-01 20:46:53 +0530

    added top module template

---
+++ TOP.v
@@ -0,0 +1,38 @@
+`include "programCounter.v"
+`include "MemoryDataRegister.v"
+`include "dataMemory.v"
+`include "instructionMemory.v"
+`include "alu.v"
+`include "instructionRegister.v"
+`include "MUXpreALU.v"
+`include "registerFile.v"
+`include "controllerFSM.v"
+
+
+module TOP(clk, rst);
+
+input wire clk, rst;
+
+
+
+controllerFSM FSM_Main();
+
+programCounter PC();
+
+instructionMemory IM();
+instructionRegister IR();
+
+dataMemory DM();
+MemoryDataRegister MDR();
+
+registerFile RF();
+
+alu ALU_MAIN();
+MUXpreALU MPA();
+
+
+
+
+
+
+endmodule
