1ff15222eaf55d2b0ca69a95a577d36e3871232e
khandelwalkshitij <kshitij14159@gmail.com>
2018-04-03 09:31:56 +0530

    tested registerFile.v (kinda)

--- testbenches/tb_registerFile.v
+++ testbenches/tb_registerFile.v
@@ -12,7 +12,7 @@ module tb_registerFile();
 	reg C_RegDstWrite, C_RegWrite, C_MemToReg;
 	reg clk, rst;
 
-	uut  registerFile(	D_ReadReg1RT, D_ReadReg2RT,
+	registerFile uut(	D_ReadReg1RT, D_ReadReg2RT,
 						D_Offset, D_RegSW,
 						D_BT,
 						D_MDR_IN, D_ALU_IN,	
@@ -31,12 +31,33 @@ module tb_registerFile();
 
 	initial
 	begin
-		$stop
+		#5 rst = 1'b1;
+		#27 rst = 1'b0; A_ReadReg1RT = 4'b0011; A_ReadReg2RT = 4'b0110; A_Offset = 4'b0010; A_RegSWLW = 4'b0111; A_WriteRegRT_BT = 4'b1110; 
+		D_MDR_IN = 16'd154; D_ALU_IN = 16'd777;   	
+		C_RegDstWrite = 1'b1; C_RegWrite = 1'b1; C_MemToReg = 1'b0;
+		#55 rst = 1'b1;
+		#27 rst = 1'b0; A_ReadReg1RT = 4'b0011; A_ReadReg2RT = 4'b0110; A_Offset = 4'b0010; A_RegSWLW = 4'b0111; A_WriteRegRT_BT = 4'b1110; 
+		D_MDR_IN = 16'd154; D_ALU_IN = 16'd777;   	
+		C_RegDstWrite = 1'b1; C_RegWrite = 1'b1; C_MemToReg = 1'b1;
+		#55 rst = 1'b1;
+		#27 rst = 1'b0; A_ReadReg1RT = 4'b0011; A_ReadReg2RT = 4'b0110; A_Offset = 4'b0010; A_RegSWLW = 4'b0111; A_WriteRegRT_BT = 4'b1110; 
+		D_MDR_IN = 16'd154; D_ALU_IN = 16'd777;   	
+		C_RegDstWrite = 1'b0; C_RegWrite = 1'b1; C_MemToReg = 1'b1;
+		#55 rst = 1'b1;
+		#27 rst = 1'b0; A_ReadReg1RT = 4'b0011; A_ReadReg2RT = 4'b0110; A_Offset = 4'b0010; A_RegSWLW = 4'b0111; A_WriteRegRT_BT = 4'b1110; 
+		D_MDR_IN = 16'd154; D_ALU_IN = 16'd777;   	
+		C_RegDstWrite = 1'b1; C_RegWrite = 1'b1; C_MemToReg = 1'b0;
+		#55 rst = 1'b1;
+		#27 rst = 1'b0; A_ReadReg1RT = 4'b0011; A_ReadReg2RT = 4'b0110; A_Offset = 4'b0010; A_RegSWLW = 4'b0111; A_WriteRegRT_BT = 4'b1110; 
+		D_MDR_IN = 16'd154; D_ALU_IN = 16'd777;   	
+		C_RegDstWrite = 1'b1; C_RegWrite = 1'b0; C_MemToReg = 1'b1;
+
+		$stop;
 	end
 
 	initial
 	begin
-		$dumpfile("alu.vcd");
+		$dumpfile("registerFile.vcd");
 		$dumpvars;
 	end
 	
